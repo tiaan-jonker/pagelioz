7048a5059d7fc52b34f49474b51bbc50faa28fc5
tiaan-jonker <tiaanj06@gmail.com>
2022-08-01 14:46:09 +1200

    User and userbook models added

---
+++ server/models/userModel.js
@@ -0,0 +1,27 @@
+import mongoose from 'mongoose'
+const { Schema } = mongoose
+
+const userSchema = new Schema(
+  {
+    name: {
+      type: String,
+      require: true,
+    },
+    email: {
+      type: String,
+      require: true,
+      unique: true,
+    },
+    password: {
+      type: String,
+      require: true,
+    },
+  },
+  {
+    timestamps: true,
+  }
+)
+
+const User = mongoose.model('User', userSchema)
+
+export default User
